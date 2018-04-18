1ef88230050d24180b8711a6c4d9de38433f1907
Suzana <suzana_ene@yahoo.com>
2018-04-18 19:32:29 +0300

    First version of the project

---
+++ src/com/company/Tipper.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public final class Tipper extends Vehicle {
+    private int capacityInTons;
+
+    public Tipper(){
+
+    }
+    public Tipper(int age, long numberOfMiles, boolean isDiesel, int capacityInTons){
+        super(age, numberOfMiles, isDiesel);
+        this.capacityInTons = capacityInTons;
+    }
+
+    public int getCapacityInTons() {
+        return capacityInTons;
+    }
+
+    public void setCapacityInTons(int capacityInTons) {
+        this.capacityInTons = capacityInTons;
+    }
+}
